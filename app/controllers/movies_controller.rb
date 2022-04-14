a5cbbf33fe109946dc74d1f94745f8cd1549e73c
Brandon Lin <bwlin93@gmail.com>
2022-04-13 20:03:06 -0500

    adds parameters to permitted list in movie controller, tests create function

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -39,13 +39,15 @@ class MoviesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_movie
-      @movie = Movie.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def movie_params
-      params.fetch(:movie, {})
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def movie_params
+    # params.fetch(:movie, {})
+    params.require(:movie).permit(:title, :plot, :year, :director)
+  end
 end
