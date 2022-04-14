ed5a52324258ce16c3c9204b6385fc3add72f6b9
Brandon Lin <bwlin93@gmail.com>
2022-04-13 19:50:40 -0500

    creates db and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Movie.create(title: "The Bourne Identity", plot: "people shooting people", year: 2001, director: "a person")
+Movie.create(title: "Finding Nemo", plot: "dad find son", year: 2003, director: "Mr. Person")
