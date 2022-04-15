fa221b186012abb90c8f1ce32ac8fe5f33c15af2
Brandon Lin <bwlin93@gmail.com>
2022-04-15 10:20:31 -0500

    adds valid params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,13 +39,15 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.fetch(:user, {})
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def user_params
+    # params.fetch(:user, {})
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
