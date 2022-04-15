8acc99c04d87bf8e0cd1a887f9f6b240a2907e2f
Brandon Lin <bwlin93@gmail.com>
2022-04-15 10:12:38 -0500

    adds users and sessions framework

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  resources :sessions
+  resources :users
   resources :movies
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
 end
